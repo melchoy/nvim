f41a42623dc0a08d73b018244849eaf0e69c4227
Melissa Choy <mel@melchoy.com>
2024-10-06 09:52:20 +1100

    ts lsp server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "ts_ls" }
       })
     end
   },
@@ -20,6 +20,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+			lspconfig.ts_ls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return  {
 			ensure_installed = {
 				"lua",
 				"javascript",
+				"typescript",
 				"ruby",
 				"go",
 				"python"
