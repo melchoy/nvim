d664fb9e043070c6c7aa9d5fae0492923b31cafd
Melissa Choy <mel@melchoy.com>
2025-09-17 23:38:41 +1000

    neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,10 +1,10 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
 	},
 	config = function()
 		require("neo-tree").setup({
@@ -21,112 +21,113 @@ return {
 			filesystem = {
 				hijack_netrw_behavior = "disabled", -- Don't hijack directory opening
 				filtered_items = {
-					visible = false,        -- Show filtered items dimmed instead of hidden
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,  -- Show git-ignored files
-          hide_by_name = {
-            -- System files
-            '.DS_Store',
-            'thumbs.db',
-            'desktop.ini',
-            
-            -- Dependencies
-            'node_modules',
-            '.npm',
-            '.yarn',
-            'bower_components',
-            'vendor',
-            '__pycache__',
-            '.venv',
-            'venv',
-            
-            -- Build outputs
-            'dist',
-            'build',
-            '.next',
-            '.nuxt',
-            'out',
-            'target',
-            '.cache',
-            'tmp',
-            'temp',
-            'log',
-            'logs',
-            
-            -- IDE/Editor
-            '.vscode',
-            '.idea',
-            '.vs',
-            '*.swp',
-            '*.swo',
-            '*~',
-          },
-          hide_by_pattern = {
-            -- Patterns
-            "*.tmp",
-            "*.log", 
-            "*.pyc",
-            "*.class",
-            "*.o",
-            "*.so",
-            "*.dll",
-            "*.exe",
-          },
-          hide_by_name = {
-            -- System files
-            '.DS_Store',
-            'thumbs.db',
-            'desktop.ini',
-            
-            -- Dependencies
-            'node_modules',
-            '.npm',
-            '.yarn',
-            'bower_components',
-            'vendor',
-            '__pycache__',
-            '.venv',
-            'venv',
-            
-            -- Build outputs
-            'dist',
-            'build',
-            '.next',
-            '.nuxt',
-            'out',
-            'target',
-            '.cache',
-            'tmp',
-            'temp',
-            
-            -- Log files
-            'logs',
-            'log',
-            
-            -- IDE/Editor
-            '.vscode',
-            '.idea',
-            '.vs',
-            '*.swp',
-            '*.swo',
-            '*~',
-          },
-          never_show = {
-            -- Never show these even if forced
-            '.git',
-            '.hg',
-            '.svn',
-          },
-        },
-        -- Toggle showing hidden files
-        follow_current_file = {
-          enabled = true,
-        },
-        use_libuv_file_watcher = true, -- Auto-refresh
-      },
-    })
+					visible = false,              -- Show filtered items dimmed instead of hidden
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = false, -- Show git-ignored files
+					hide_by_name = {
+						-- System files
+						'.DS_Store',
+						'thumbs.db',
+						'desktop.ini',
+
+						-- Dependencies
+						'node_modules',
+						'node-compile-cache',
+						'.pnpm-store',
+						'.npm',
+						'.yarn',
+						'bower_components',
+						'vendor',
+						'__pycache__',
+						'.venv',
+						'venv',
+
+						-- Build outputs
+						'dist',
+						'build',
+						'.next',
+						'.nuxt',
+						'out',
+						'target',
+						'.cache',
+						'tmp',
+						'temp',
+						'log',
+						'logs',
+
+						-- IDE/Editor
+						'.vscode',
+						'.idea',
+						'.vs',
+						'*.swp',
+						'*.swo',
+						'*~',
+					},
+					hide_by_pattern = {
+						-- Patterns
+						"*.tmp",
+						"*.log",
+						"*.pyc",
+						"*.class",
+						"*.o",
+						"*.so",
+						"*.dll",
+						"*.exe",
+					},
+					hide_by_name = {
+						-- System files
+						'.DS_Store',
+						'thumbs.db',
+						'desktop.ini',
+
+						-- Dependencies
+						'node_modules',
+						'.npm',
+						'.yarn',
+						'bower_components',
+						'vendor',
+						'__pycache__',
+						'.venv',
+						'venv',
+
+						-- Build outputs
+						'dist',
+						'build',
+						'.next',
+						'.nuxt',
+						'out',
+						'target',
+						'.cache',
+						'tmp',
+						'temp',
+
+						-- Log files
+						'logs',
+						'log',
+
+						-- IDE/Editor
+						'.vscode',
+						'.idea',
+						'.vs',
+						'*.swp',
+						'*.swo',
+						'*~',
+					},
+					never_show = {
+						-- Never show these even if forced
+						'.git',
+						'.hg',
+						'.svn',
+					},
+				},
+				-- Toggle showing hidden files
+				follow_current_file = {
+					enabled = true,
+				},
+				use_libuv_file_watcher = true, -- Auto-refresh
+			},
+		})
 		vim.keymap.set('n', '<C-t>', ':Neotree filesystem toggle left<CR>', { desc = "Toggle file tree" })
 	end
 }
-
